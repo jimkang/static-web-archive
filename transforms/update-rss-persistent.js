c43f4af81e7b3da8824796828420e9fe88c31d60
Jim Kang <jimkang@gmail.com>
2020-09-29 21:17:20 -0400

    Be loud about RSS errors.

--- transforms/update-rss-persistent.js
+++ transforms/update-rss-persistent.js
@@ -20,21 +20,7 @@ function UpdateRSSPersistent({
   // RSS-updating thing based on the cells in the persistent last page.
   // This is another point in favor of refactoring to not use the stream model.
   function updateRSSPersistent(cell, enc, updateDone) {
-    setTimeout(() => updateRSS(passCell), delay);
-
-    function passCell(error) {
-      if (error) {
-        if (error.name === 'NoRecentPageError') {
-          // This is OK, and not a reason to stop the stream.
-          console.error(error);
-          updateDone(null, cell);
-        } else {
-          updateDone(error);
-        }
-      } else {
-        updateDone(null, cell);
-      }
-    }
+    setTimeout(() => updateRSS(updateDone), delay);
   }
 }
 
